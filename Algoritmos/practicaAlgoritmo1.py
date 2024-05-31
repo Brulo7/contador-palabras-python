c3dc562a4b57389e2a2ddc8dcc472a2fd8d2c684
Brulo7 <lorenzobruni55@gmail.com>
2024-05-31 19:01:29 -0300

    contador-palabras

---
+++ Algoritmos/practicaAlgoritmo1.py
@@ -0,0 +1,13 @@
+### practica de pasar a python lo del pseudocodigo
+
+
+###paso 1:
+from itertools import count
+
+
+print("Vamos a contar la cantidad de letras que tiene una palabra.")
+palabra_ingresada = input("ingrese una palabra: ")
+
+cantidad_letras = len(palabra_ingresada)
+
+print("La palabra ingresada tiene ", cantidad_letras, "letras")
